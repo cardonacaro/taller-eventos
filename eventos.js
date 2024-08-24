01c3a4099a01715bcc98353ff3b9fb71919c6025
Caro <caro.ssj28@gmail.com>
2024-08-23 23:53:03 -0300

    evento en div

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const myDiv = document.getElementById('myDiv');
+        divBtn.addEventListener('click', function() {
+            alert('Hola! Soy el div');
+        });
+    });
